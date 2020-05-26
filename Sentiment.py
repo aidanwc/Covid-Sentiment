8dbd7c6e1888a3987cb82051aa1be041caabeb94
Aidan Weber-Concannon <aidanwc98@gmail.com>
2020-05-25 22:11:58 -0400

    added hourly scores

--- Sentiment.py
+++ Sentiment.py
@@ -12,7 +12,7 @@ import datetime
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tracker.settings')
 django.setup()
-from sentiment.models import DailyScore,Tweet
+from sentiment.models import DailyScore,Tweet, HourlyScore
 from django.db.models import Model
 
 consumer_key = config('consumer_key')
@@ -25,7 +25,7 @@ hashtag_phrase = "Covid"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 analyzer = SentimentIntensityAnalyzer()
 
@@ -54,26 +54,54 @@ def sentiment_script():
 
                 final = final.append(df)
 
-        mean = round(final.mean(),3)
-
+        hourly_mean = round(final.mean(),3)
+       
+        print()
         print(final)
-        print(mean)
+        print(hourly_mean)
 
         numberPositive = str(final.Sentiment.str.count("Positive").sum())
         numberNegative = str(final.Sentiment.str.count("Negative").sum())
         numberNeutral = str(final.Sentiment.str.count("Neutral").sum())
 
-        print("")
+        
 
         print("Number of positive tweets: " + numberPositive)
         print("Number of negative tweets: " + numberNegative)
-        print("Number of neutral tweets: " + numberNeutral)
+        print("Number of neutral tweets: \n" + numberNeutral)
         
-        #todays_score = DailyScore(score=mean)
-        #todays_score.save()
+        hourly_score = HourlyScore(score=hourly_mean, positiveCount=numberPositive, negativeCount=numberNegative, neutralCount=numberNeutral)
+        hourly_score.save()
 
-#Running the script
+        now = datetime.datetime.now()
+        
+        print("Today's date: {}".format(now.date()))
+        print('Current time: {}\n'.format(now.hour))
+        print(datetime.date.today())
+        
+        if(now.hour == 23):
+                allScores = HourlyScore.objects.filter(date=datetime.date.today())
+                counter=0
+                scoreSum=0
+                posSum=0
+                negSum=0
+                neutSum=0
+                
+                
+                for score in allScores:
+                        counter+=1
+                        scoreSum+=score.score
+                        posSum+=score.positiveCount
+                        negSum+=score.negativeCount
+                        neutSum+=score.neutralCount
+                samplemean= scoreSum/min(1,counter)
+                samplemean=round(samplemean,3)
+                print("Number of hourly scores:{} SumScores:{} Sample Mean:{} \n positiveTweets:{} negativeTweets:{} neutral tweets: {}".
+                      format(counter,scoreSum,samplemean,posSum,negSum,neutSum))
+                
+                daily_score=DailyScore(score=samplemean, positiveCount=posSum, negativeCount=negSum, neutralCount=neutSum)
+                daily_score.save()
 
-now = datetime.datetime.now()
 
-if(now.hour == 23):
+#Running Script 
+sentiment_script()
