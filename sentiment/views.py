7559046fc4c9659e87962d42e45f9a7ed8ac5839
Aidan Weber-Concannon <aidanwc98@gmail.com>
2020-05-27 23:38:38 -0400

    added charts

--- sentiment/views.py
+++ sentiment/views.py
@@ -5,13 +5,21 @@ def index(request):
     context={}
     scores=[]
     dates=[]
+    positive=[]
+    negative=[]
+    neutral=[]
     data= DailyScore.objects.order_by('date')
    
     for item in data:
         scores.append(item.score)
         dates.append(str(item.date))
-        print(str(item.date))
+        positive.append(item.positiveCount)
+        negative.append(item.negativeCount)
+        neutral.append(item.neutralCount)
         
     context['scores']=scores
     context['dates']=dates 
+    context['positive']=positive
+    context['negative']=negative
+    context['neutral']=neutral
     return render(request,'sentiment/index.html',context)
